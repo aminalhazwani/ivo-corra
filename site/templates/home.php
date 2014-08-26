a8e07959d359b2cc6848874e71cd6c0f82561607
aminalhazwani <amin.alhazwani@gmail.com>
2014-08-26 15:08:41 +0200

    Updated home.php foreach loop

--- site/templates/home.php
+++ site/templates/home.php
@@ -6,17 +6,17 @@
 <?php snippet('contact') ?>
 
 <div class="container">
-    <?php foreach($pages->find('works')->children()->visible() as $work): ?>
-        <?php if($work->home() == 'ja'): ?>
-            <div class="work">   
-                <?php foreach($work->images() as $image): ?>
-                    <a class="box" href="<?php echo $image->url() ?>" data-lightbox="<?php echo $work->title() ?>">
-                        <!-- <div class="overlay"></div> -->
-                        <img src="<?php echo thumb($image, array('width' => 300), false) ?>" alt="<?php echo $work->title() ?>">
-                    </a>
-                <?php endforeach ?>
-            </div>
-        <?php endif ?>
+    <?php foreach($pages->find('works')->children() as $work): ?>
+    <?php if($work->home() == 'ja'): ?>
+        <div class="work"> 
+            <?php foreach($work->images() as $image): ?>
+                <a class="box" href="<?php echo $image->url() ?>" data-lightbox="<?php echo $work->title() ?>">
+                    <!-- <div class="overlay"></div> -->
+                    <img src="<?php echo thumb($image, array('width' => 300), false) ?>" alt="<?php echo $work->title() ?>">
+                </a>
+            <?php endforeach ?>
+        </div>
+    <?php endif ?>
     <?php endforeach ?>
 </div>
 
