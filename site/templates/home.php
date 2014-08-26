f7a1eb96a454303c31aea38ccfe96340e14e9bf6
aminalhazwani <amin.alhazwani@gmail.com>
2014-08-26 17:50:41 +0200

    Minor changes home.php

--- site/templates/home.php
+++ site/templates/home.php
@@ -12,7 +12,7 @@
             <?php foreach($work->images() as $image): ?>
                 <a class="box" href="<?php echo $image->url() ?>" data-lightbox="<?php echo $work->title() ?>">
                     <!-- <div class="overlay"></div> -->
-                    <img src="<?php echo $image->url() ?>" alt="<?php echo $work->title() ?>">
+                    <img src="<?php echo thumb($image, array('width' => 300), false) ?>" alt="<?php echo $work->title() ?>">
                 </a>
             <?php endforeach ?>
         </div>
