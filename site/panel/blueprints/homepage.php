d00571eecf0e60d6bd62738c6714bfa0af95ee90
aminalhazwani <amin.alhazwani@gmail.com>
2014-09-03 09:07:25 +0200

    New homepage created

---
+++ site/panel/blueprints/homepage.php
@@ -0,0 +1,12 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+# default blueprint
+
+title: Page
+pages: 
+    template: homepageproject
+files: false
+fields:
+    title: 
+        label: Titolo pagina
+        type:  text
---
+++ site/panel/blueprints/homepageproject.php
@@ -0,0 +1,16 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+# works blueprint
+
+title: Page
+pages: false
+files: true
+fields:
+    title: 
+        label: Titolo progetto
+        type:  text
+    text: 
+        label: Testo del progetto
+        type:  textarea
+        size:  large
+        buttons: true
