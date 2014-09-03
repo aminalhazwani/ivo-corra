45a0b447bb7f3e8d6eb32b643c6c13e8160ed8b8
aminalhazwani <amin.alhazwani@gmail.com>
2014-09-03 10:14:48 +0200

    Added new foreach loop for the homepage

--- site/templates/home.php
+++ site/templates/home.php
@@ -5,19 +5,19 @@
 <?php snippet('menu') ?>
 <?php snippet('contact') ?>
 
+<?php snippet('spinner') ?>
+
 <div class="container">
-    <?php foreach($pages->find('works')->children()->visible() as $work): ?>
-    <?php if($work->home() == 'ja'): ?>
-        <div class="work"> 
-            <?php foreach($work->images() as $image): ?>
-                <a class="box" href="<?php echo $image->url() ?>" data-lightbox="<?php echo $work->title() ?>">
-                    <!-- <div class="overlay"></div> -->
-                    <img src="<?php echo thumb($image, array('width' => 300), false) ?>" alt="<?php echo $work->title() ?>">
-                </a>
-            <?php endforeach ?>
-        </div>
-    <?php endif ?>
+    <?php foreach($pages->find('homepage')->children()->visible() as $work): ?>
+    <div class="work"> 
+        <?php foreach($work->images() as $image): ?>
+            <a class="box" href="<?php echo $image->url() ?>" data-lightbox="<?php echo $work->title() ?>">
+                <!-- <div class="overlay"></div> -->
+                <img src="<?php echo thumb($image, array('width' => 300), false) ?>" alt="<?php echo $work->title() ?>">
+            </a>
+        <?php endforeach ?>
+    </div>
     <?php endforeach ?>
 </div>
-
+ 
 <?php snippet('foot') ?>
