9863415223527640b31c0fd95791618ab68b1d58
aminalhazwani <amin.alhazwani@gmail.com>
2014-09-03 10:13:29 +0200

    Added spinner on page load to the body footer

--- site/snippets/foot.php
+++ site/snippets/foot.php
@@ -1,3 +1,8 @@
-	
+    <script type="text/javascript">
+        $(window).load(function() {
+            $('.spinner__container').fadeOut(1000);
+        });
+    </script>	
+    
 	</body>
 </html>
