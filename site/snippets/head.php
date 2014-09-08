90cf2780a6486373fab12ec0749c488c5c3333ac
aminalhazwani <amin.alhazwani@gmail.com>
2014-09-08 16:25:46 +0200

    Added new favicon

--- site/snippets/head.php
+++ site/snippets/head.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <meta name="format-detection" content="telephone=no">
 
-    <link rel="icon" type="image/png" href="<?php echo url() ?>/assets/images/fav-icon.png">
+    <link rel="icon" type="image/png" href="<?php echo url() ?>/assets/images/favicon.png">
 
     <?php echo css('assets/styles/main.css') ?>
 
